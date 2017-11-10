5fddace7472f45354f3fe6ef1661507e89e26cf9
NicklasRohman <NicklasRohman@gmail.com>
2017-11-10 15:26:35 +0100

    secund push

--- src/backing/SportsingBean.java
+++ src/backing/SportsingBean.java
@@ -1,31 +1,34 @@
 package backing;
 
-import javax.ejb.EJB;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import com.sportsing.api.Match;
-import entities.MatchResult;
-import facades.MatchResultFacade;
 
 @Named
 @ViewScoped
-public class SportsingBean {
-
-	@EJB
-	private MatchResultFacade matchEJB;
-
-	public void updateMatchResult() {
+public class SportsingBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private List<Match>matchResults;
+	
+	public List<Match> getMatchResult(){
+		matchResults = new ArrayList<>();
 		WebTarget matchTarget = ClientBuilder.newClient()
-				.target("http://localhost:8080/sportsing-webservice/rs/sports/");
-
-		for (MatchResult m : matchEJB.findAll()) {
-			WebTarget target = matchTarget.path("get/" + m.getContenderIterator());
-			Match webServiceMatch = target.request(MediaType.APPLICATION_XML).get(Match.class);
-			webServiceMatch.registerContender("Nicklas", 1, 3);
-			matchEJB.edit(m);
-		}
+				.target("http://localhost:8080/sportsing-webservice/rs/sports/list");	
+		matchResults = matchTarget.request(MediaType.APPLICATION_XML).get(Match.class).getMatches();
+	return matchResults;
+	
 	}
+
+	public void setMatchResult(List<Match> matchResults) {
+		this.matchResults = matchResults;
+	}
+	
 }
---
+++ src/entities/Match.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import com.sportsing.api.ContenderResult;
+
+	@XmlRootElement
+	public class Match implements Serializable{
+		private static final long serialVersionUID = 1L;
+		
+		@XmlElement
+		private String sport;
+
+		@XmlElementWrapper(name="contenders")
+		@XmlElement(name="contender")
+		private List<ContenderResult> contenders;
+
+		public Match() {}
+		
+		public Match(String sport,String name,int score,int place) {
+			this.sport = sport;
+			contenders = new ArrayList<ContenderResult>();
+			registerContender(name, score, place);
+		}
+		public void registerContender(ContenderResult cr) {
+			contenders.add(cr);
+		}
+		public void registerContender(String name, float score, int place) {
+			registerContender(new ContenderResult(name, score, place));
+		}
+
+		public Iterator<ContenderResult> getContenderIterator() {
+			return contenders.iterator();
+		}
+		
+}
