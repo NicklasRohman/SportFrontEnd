0e2f9615e7f4c8f7e1e62418ae248b960b51a21b
NicklasRohman <NicklasRohman@gmail.com>
2017-11-14 09:52:13 +0100

    Starting with DB

--- src/backing/SportsingBean.java
+++ src/backing/SportsingBean.java
@@ -13,28 +13,18 @@ import com.sportsing.api.Match;
 
 @Named
 @ViewScoped
-public class SportsingBean implements Serializable{
+public class SportsingBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	List<Match> matchResult = new ArrayList<>();
-	
-	public List<Match> getMatchResult(){
 
-		
-		
+	public List<Match> getMatchResult() {
+
 		WebTarget matchTarget = ClientBuilder.newClient()
-				.target("http://localhost:8080/sportsing-webservice/rs/sports/list");	
+				.target("http://localhost:8080/sportsing-webservice/rs/sports/list");
 
-		matchResult = matchTarget.request(MediaType.APPLICATION_XML).get(new GenericType<List<Match>>() {});
-		
+		matchResult = matchTarget.request(MediaType.APPLICATION_XML).get(new GenericType<List<Match>>() {
+		});
+
 		return matchResult;
 	}
 }
-
-
-
-
-
-
-
-
-
